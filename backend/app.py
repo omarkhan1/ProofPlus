64382506e39501cfca4b36c8e8f6a7df6b6c9adf
Ethan Concannon <econcann@umich.edu>
2023-03-16 15:38:12 -0400

    handle audio files sent

--- backend/app.py
+++ backend/app.py
@@ -1,16 +1,13 @@
-from flask import Flask, jsonify, flash, request, redirect, url_for, session
-# from flask.ext.session import Session
+from flask import Flask, jsonify, request
 from flask_restful import Api, Resource
 from dataclasses import dataclass
 from werkzeug.utils import secure_filename
+import os
 
-# UPLOAD_FOLDER = '/uploads'
 ALLOWED_EXTENSIONS = {'mp3', 'mp4'}
 
 app = Flask(__name__)
-# app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 app.config.from_pyfile('config.py')
-# sess = Session()
 api = Api(app)
 
 
@@ -18,27 +15,23 @@ api = Api(app)
 class Location:
     chapter: int
     verse: int
-    
-def allowed_file(filename):
-    return '.' in filename and \
-        filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 
 class Prediction(Resource):
     def post(self):
-        # if 'file' not in request.files:
-        #     flash('No file part')
-        #     return redirect(request.url)
-        # file = request.files['file']
-        # # If the user does not select a file, the browser submits an
-        # # empty file without a filename.
-        # if file.filename == '':
-        #     flash('No selected file')
-        #     return redirect(request.url)
-        # if file and allowed_file(file.filename):
-        #     filename = secure_filename(file.filename)
-        #     file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-        #     return redirect(url_for('download_file', name=filename))
+        # no file sent
+        if 'file' not in request.files:
+            return jsonify({"location_0": Location(-2, -1)})
+        
+        file = request.files['file']
+        # file is not an mp3 or mp4
+        _, file_extension = os.path.splitext(file.filename)
+        print(file_extension)
+        if file_extension not in ['.mp3', '.mp4']:
+            return jsonify({"location_0": Location(-1, -1)})
+        
+        filename = secure_filename(file.filename)
+        # file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
         return jsonify({"location_1": Location(1, 1), 
                         "location_2": Location(2, 2), 
                         "location_3": Location(3, 3)})
@@ -46,21 +39,10 @@ class Prediction(Resource):
 
 @app.route('/')
 def home():
-    return "Flask heroku app."
+    return "ProofPlus is up and running."
 
 api.add_resource(Prediction, '/predict')
 
-# @app.route("/predict", methods=["POST"])
-# def predict():
-#     return jsonify({"location_1": (1, 1), "location_2": (2, 2), "location_3": (3, 3)})
-
 
 if __name__ == "__main__":
-    # app.secret_key = 'super secret key'
-    # app.config['SESSION_TYPE'] = 'filesystem'
-
-    # sess.init_app(app)
-
-    # app.debug = True
-    # app.run()
     app.run()
--- backend/config.py
+++ backend/config.py
@@ -1,4 +1,4 @@
 import os
 
 SECRET_KEY = os.urandom(24)
-UPLOAD_FOLDER = '/uploads'
+UPLOAD_FOLDER = os.getcwd() + "/uploads"
