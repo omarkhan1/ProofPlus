9d28827e1cea56c903c1227ae7271f11d13b0262
Ethan Concannon <econcann@umich.edu>
2023-03-16 00:53:00 -0400

    test post

--- backend/app.py
+++ backend/app.py
@@ -1,20 +1,27 @@
 from flask import Flask, jsonify
 from flask_restful import Api, Resource
+from dataclasses import dataclass
 
 app = Flask(__name__)
 api = Api(app)
 
 
+@dataclass
+class Location:
+    chapter: int
+    verse: int
+
+
 class Prediction(Resource):
-    def get(self):
-        return jsonify({"location_1": (1, 1), "location_2": (2, 2), "location_3": (3, 3)})
+    def post(self):
+        return jsonify({"location_1": Location(1, 1), "location_2": Location(2, 2), "location_3": Location(3, 3)})
 
 
 @app.route('/')
 def home():
     return "Flask heroku app."
 
-api.add_resource(Prediction, '/prediction')
+api.add_resource(Prediction, '/predict')
 
 # @app.route("/predict", methods=["POST"])
 # def predict():
