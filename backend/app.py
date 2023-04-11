9221606278acc01c32857531b8159ad046c1ea58
Ethan Concannon <econcann@umich.edu>
2023-04-11 12:14:03 -0400

    remove email stuff

--- backend/app.py
+++ backend/app.py
@@ -3,9 +3,6 @@ from flask import Flask, jsonify, request
 from flask_restful import Api, Resource
 from werkzeug.utils import secure_filename
 from predictor import predict, Location
-import firebase_admin
-from firebase_admin import credentials
-from firebase_admin import db
 
 
 app = Flask(__name__)
@@ -39,46 +36,12 @@ class Prediction(Resource):
                         "location_2": location_2, 
                         "location_3": location_3})
         
-        
-class EmailClient(Resource):
-    # add emails from frontend so that we can send users emails
-    def post(self):
-        if not firebase_admin._apps:
-            cred = credentials.Certificate("cred.json")
-            firebase_admin.initialize_app(cred, {
-                "databaseURL": "https://proofplus-14b46-default-rtdb.firebaseio.com/"
-            })
-        ref = db.reference("/email")
-        
-        data = request.json
-        ref.push(data)
-        return
-
-
-class VerseCounter(Resource):
-    # increase count for verse each time it is selected
-    def post(self):
-        if not firebase_admin._apps:
-            cred = credentials.Certificate("cred.json")
-            firebase_admin.initialize_app(cred, {
-                "databaseURL": "https://proofplus-14b46-default-rtdb.firebaseio.com/"
-            })
-            
-        data = request.json
-        verse = int(data["verse"])
-        ref = db.reference(f"/verses/{verse}")
-        curr_count = int(ref.get())
-        ref.set(curr_count + 1)
-        return
-
 
 @app.route('/')
 def home():
     return "ProofPlus is up and running."
 
 api.add_resource(Prediction, '/predict')
-api.add_resource(EmailClient, "/email")
-api.add_resource(VerseCounter, "/verse")
 
 
 if __name__ == "__main__":
